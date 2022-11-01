298124bf60442a4b4487361d534370f90f7d505f
Ben Pettis <bpettis@wisc.edu>
2022-11-01 13:57:11 -0500

    add new grammars

--- js/app.js
+++ js/app.js
@@ -18,9 +18,18 @@ grammar = tracery.createGrammar({
         "gamification",
         "climate change",
         "digital disembedding",
-        "political polarization"
+        "political polarization",
+        "radicalization",
+        "conspiratorial thinking",
+        "datafication"
     ],
-    concept: ["media literacy", "media production", "media labor", "identity", "gender", "nationality", "media effects research project", "ethics of care"],
+    organization: [
+        'The Federal Communications Commissions',
+        'The Motion Picture Production Association of America',
+        'The European Union',
+        'The White House'
+    ],
+    concept: ["media literacy", "media production", "media labor", "identity", "gender", "nationality", "media effects research project", "ethics of care", "digital native", "race", "stereotype", "media conglomeration", 'regulatory capture'],
     medium: ["television", "social media", "game", "video game", "app"],
     era1: ["modern", "postmodern", "premodern", "contemporary"],
     era2: ["future", "past", "not-so-distant past"],
@@ -32,9 +41,9 @@ grammar = tracery.createGrammar({
     positiveResponse: ["celebrate", "approve", "agrees with"],
     theorist: ["Marx", "Gramsci", "Judith Butler", "Louis Althusser", "Foucault", "Gilles Deleuze"],
     framework: ["critical", "feminist", "psychoanalytical", "critical code studies", "media industries", "critical political economy"],
-    method: ["Critical Technocultural Discourse Analysis", "focus groups", "a series of qualtrics surveys", "ethnography", "autoethnography"],
-    studyVerb: ["study", "examine", "critique", "analyze", "interrogate"],
-    object: ["<em>#text#</em>", "#platform#"],
+    method: ["Critical Technocultural Discourse Analysis", "focus groups", "a series of qualtrics surveys", "ethnography", "autoethnography", "semi-structured interviews"],
+    studyVerb: ["study", "examine", "critique", "analyze", "interrogate", "consider", "question"],
+    object: ["<em>#text#</em>", "#platform#", "#organization#"],
     titles: [
         "Revisiting #theorist# in the 21st Century: A #framework.capitalize# analysis.",
         "Why study #theorist# anyway?",
@@ -54,19 +63,27 @@ grammar = tracery.createGrammar({
         "Is #concept# real? #theorist# #positiveResponse.s# to #era1# #medium#",
         "The #era2# of #medium.s#: Envisioning #concept# after #year#",
         "\"#oneliner#\" #celebrity# said what!?",
-        "Using #theorist# to #studyVerb# the production of #concept# in #medium.s#"
+        "Using #theorist# to #studyVerb# the production of #concept# in #medium.s#",
+        "Should #object# be involved with #phenomenon# after #year#?: The effects of #concept# on #medium.s#",
+        "#celebrity#, #publication#, and #object#: An Exploratory Analysis",
+        "#object# after #year#: Toward #framework.a# understanding of #medium.s#",
+        "Revisiting #theorist# after #year1#: Toward #era1.a# #framework# analysis of #medium.s#"
     ],
     stakes: [
         "#object# is representative of the contemporary reality of political polarization",
         "scholars must pay more attention to #object#",
         "#object# shows that scholars must return to #platform#",
-        "despite the increased frequency of #phenomenon#, it remains important that we not lose sight of the important gains that have been made since #year#.",
-        "#celebrity# and #theorist# are most similar than one might initially think."
+        "despite the increased frequency of #phenomenon#, it remains important that we not lose sight of the important gains that have been made since #year#",
+        "#celebrity# and #theorist# are most similar than one might initially think", 
+        "scholars have significant power to #negativeResponse# about #medium.s# and indeed have the moral responsibility to do so",
+        "#phenomenon# represents a looming epistemological crisis that remains under-acknowledged",
+        "#object.s# offer a valuable perspective for understanding the #era2# of #era1# #medium.s#"
     ],
     abstracts: [
         "This paper uses #framework# methods to study #object# as an example of #phenomenon# in the #era1# era. In #year#, #celebrity.capitalize# was criticized for making comments about <em>#text#</em> in a widely-read opinion piece in <em>#publication.capitalize#</em>. In response, thousands of #platform# users #negativeResponse.ed#, temporarily causing the website to crash. This paper uses #method# to #studyVerb# this phenomenon and argues that #stakes#.",
         "Since #year# it has become increasingly apparent that #celebrity# has had an outspoken influence on #concept#. But this relationship is not limited to #era1# media contexts. As early as #year1# early instances of #phenomenon# could be seen emerging with an orientation toward the #era2#. In this paper, I argue that #stakes#. To do this, the paper draws upon #framework# frameworks to inform its use of #method# to critically examine #object#.",
-        "Though using #theorist# has become passé ever since #year#, the recent uptick of #celebrity# using #platform# necessitates a reconsideration. From #year# through #year1#, many have #negativeResponse.ed# the growth of #phenomenon#. #oneliner# is a common refrain found in many #publication# interviews. Accordingly, this paper uses #framework# approaches to #method# as a way to #studyVerb# #object#. By doing so, the paper argues that #stakes#."
+        "Though using #theorist# has become passé ever since #year#, the recent uptick of #celebrity# using #platform# necessitates a reconsideration. From #year# through #year1#, many have #negativeResponse.ed# the growth of #phenomenon#. #oneliner# is a common refrain found in many #publication# interviews. Accordingly, this paper uses #framework# approaches to #method# as a way to #studyVerb# #object#. By doing so, the paper argues that #stakes#.",
+        "<em>#text.capitalize#</em> has been extensively #negativeResponse.ed# by #celebrity# in <em>#publication#</em>. Despite this attention, scholars have yet to apply #framework# #method# to #studyVerb# #object#. This paper argues that by doing so, it may be possible to develop a new perspective on #era2# #medium.s#. Ultimately, I demonstrate that #stake#."
     ]
 });
 grammar.addModifiers(baseEngModifiers);
